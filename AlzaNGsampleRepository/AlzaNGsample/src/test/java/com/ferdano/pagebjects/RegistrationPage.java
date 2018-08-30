f5b01372ba284da58a8cbd7cd446f579bfffc23d
Ondrej Ferdan <ferdan.ondrej@gmail.com>
2018-08-30 13:10:42 +0200

    maven pom, testng xml, registration page a test

---
+++ AlzaNGsampleRepository/AlzaNGsample/src/test/java/com/ferdano/pagebjects/RegistrationPage.java
@@ -0,0 +1,12 @@
+package com.ferdano.pagebjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class RegistrationPage {
+
+	WebDriver driver;
+	
+	@FindBy(id="edth1EmailLogin");
+	WebElement
+	
+}
